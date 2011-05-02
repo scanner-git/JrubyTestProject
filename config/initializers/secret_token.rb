1e2194d093ceda1b9a327098e1e8f31440da5fdc
Rainer <Rainer@kibid.de>
2011-05-02 19:23:17 +0200

    Die erste Sicherung. Enthaelt das leere Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyTestProject::Application.config.secret_token = '301c2c1782c7fe3e72025fe5342c0e0cb8d6318c7c1ebdddf597cc907da216789319f8dc7ab30491263b76c077f0b4dc88100b2d03c2205c853eaeccdd9ec0e8'
